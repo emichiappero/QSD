c96f05f4b2ae62e202d0ccef2f9e4684d8a96b68
emichiappero <emichiappero@gmail.com>
2023-02-28 12:56:02 -0300

    actualización

--- sesion_16/script.js
+++ sesion_16/script.js
@@ -19,6 +19,7 @@ switch (x) {
     alert("El valor es otro numero");
 }*/
 
+/*
 var mes = prompt("Ingrese una opción: \n 1 - Suma \n 2 - Resta");
 
 switch (mes) {
@@ -37,6 +38,7 @@ switch (mes) {
   default:
     alert("El mes tiene más de 28 días");
 }
+*/
 
 /*
 Tomar el codigo de la calculadora, y hacer un "menú" donde si el usuario ingresa:
@@ -46,4 +48,55 @@ Tomar el codigo de la calculadora, y hacer un "menú" donde si el usuario ingres
  - Opción 4 ---> División
  - Otro ---> Mensaje de error
 
- */
+*/
+
+var menu = parseInt(
+  prompt(
+    "Ingrese una opción: \n\n 1)Suma \n 2)Resta \n 3) Multiplicación \n 4) División"
+  )
+);
+
+if (menu >= 1 && menu <= 4) {
+  var num1 = parseFloat(prompt("Ingrese el primer número"));
+  var num2 = parseFloat(prompt("Ingrese el segundo número"));
+}
+var total;
+var operacion;
+
+switch (menu) {
+  case 1:
+    //Suma
+    total = num1 + num2;
+    operacion = "suma";
+    break;
+
+  case 2:
+    //Resta
+    total = num1 - num2;
+    operacion = "resta";
+    break;
+
+  case 3:
+    //Multiplicación
+    total = num1 * num2;
+    operacion = "multiplicación";
+    break;
+
+  case 4:
+    //Divisón
+    if (num2 != 0) {
+      total = num1 / num2;
+      operacion = "división";
+    } else {
+      alert("No se puede dividir por cero");
+    }
+
+    break;
+
+  default:
+    alert("No ingresó una opción válida");
+}
+
+if (menu >= 1 && menu <= 4) {
+  alert("El resultado de la " + operacion + " es igual a " + total);
+}
